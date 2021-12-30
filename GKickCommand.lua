1a64cc6c4d6b57c0ac1e5702c83daf0cf16f0fc9
MarkW <numedain@gmail.com>
2021-12-30 16:57:16 +0100

    simplify the code

--- GKickCommand.lua
+++ GKickCommand.lua
@@ -1,11 +1,3 @@
--- upvalue the globals
-local _G = getfenv(0);
-local AUTOCOMPLETE_LIST = _G.AUTOCOMPLETE_LIST;
-local SlashCmdList = _G.SlashCmdList;
-local print = _G.print;
-local ChatFrame_OpenChat = _G.ChatFrame_OpenChat;
-local C_Timer__After = _G.C_Timer.After;
-
 local command = _G['SLASH_GUILD_UNINVITE1'];
 
 AUTOCOMPLETE_LIST['GKICK'] = AUTOCOMPLETE_LIST.GUILD_UNINVITE;
@@ -13,7 +5,7 @@ AUTOCOMPLETE_LIST['GKICK'] = AUTOCOMPLETE_LIST.GUILD_UNINVITE;
 _G.SLASH_GKICK1 = '/gkick';
 SlashCmdList['GKICK'] = function(msg)
 	print('The corrected command will be filled into your chat box in a second...');
-	C_Timer__After(1, function()
+	C_Timer.After(1, function()
 		ChatFrame_OpenChat(command .. ' ' .. msg);
 	end);
 end;
